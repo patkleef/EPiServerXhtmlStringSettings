d342cb9125042b45810486c1c64ab2ba4428afa4
unknown <PatrickKl@PC254.macaw.nl>
2015-03-15 15:29:06 +0100

    First commit

---
+++ EPiServerXhtmlStringSettings/Business/EditorDescriptors/CustomXhtmlStringEditorDescriptor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.Cms.Shell.UI.ObjectEditing.EditorDescriptors;
+using EPiServer.Core;
+using EPiServer.Shell.ObjectEditing.EditorDescriptors;
+
+namespace EPiServerXhtmlStringSettings.Business.EditorDescriptors
+{
+    [EditorDescriptorRegistration(TargetType = typeof (XhtmlString), UIHint = "custom-xhtmlstring")]
+    public class CustomXhtmlStringEditorDescriptor : XhtmlStringEditorDescriptor
+    {
+        public CustomXhtmlStringEditorDescriptor() : base()
+        {
+
+        }
+
+        public override void ModifyMetadata(EPiServer.Shell.ObjectEditing.ExtendedMetadata metadata,
+            IEnumerable<Attribute> attributes)
+        {
+            base.ModifyMetadata(metadata, attributes);
+
+            dynamic data = metadata.CustomEditorSettings["uiParams"];
+            var dictionary = (Dictionary<string, object>) data.GetType().GetProperty("inlineSettings").GetValue(data, null);
+            //dictionary["body_class"] = dictionary["body_class"] + " container";
+        }
+    }
+}
---
+++ EPiServerXhtmlStringSettings/Models/Pages/HomePage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EPiServerXhtmlStringSettings.Models.Pages
+{
+    [ContentType(DisplayName = "HomePage", GUID = "492924e5-7d6d-4757-8411-79a2b722ac78", Description = "")]
+    public class HomePage : PageData
+    {
+        [UIHint("custom-xhtmlstring")]
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
